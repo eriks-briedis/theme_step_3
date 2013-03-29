6448c2b9cc4841d27354cbf9874f8d2341f80b23
eriks-briedis <eriks.briedis@gmail.com>
2013-03-29 20:49:31 +0100

    sidebar

--- sidebar.php
+++ sidebar.php
@@ -4,8 +4,8 @@ This is a sidebar
 *****************/
 ?>
 
-<?php if(is_active_sidebar('sidebar-main')) : //let's check if the widget area is defined in functions.php ?>
+<?php if(is_active_sidebar('main-sidebar')) : //let's check if the widget area is defined in functions.php ?>
 	<div class="sidebar">
-		<?php dynamic_sidebar('sidebar-main'); //including the widget area ?>
+		<?php dynamic_sidebar('main-sidebar'); //including the widget area ?>
 	</div>
 <?php endif; ?>
